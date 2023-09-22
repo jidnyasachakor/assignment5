ee3b400bbd611663b9314a06e57ee2f275dd2276
jidnyasachakor <145314078+jidnyasachakor@users.noreply.github.com>
2023-09-22 17:27:28 +0530

    Add files via upload

---
+++ mov.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Enter hero name"
+read hero
+
+echo "Enter movie name"
+read movie
+
+echo "Enter language"
+read language
+
+echo "Enter price"
+read price
+
+echo "enter date of release"
+read date
+
+echo "file= $hero $movie $language $price $date"
